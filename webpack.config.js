0463c9287c75152fa346097275d8287753fcda30
AdamiSheff <adamisheff@gmail.com>
2021-10-01 21:51:51 -0400

    Completed file structure, package.json, webpack config. Succesful test of webpack build!

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/i,
+        use: ['style-loader','css-loader','sass-loader']
+      },
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    hot: true,
+    port: 8080,
+    proxy: {
+      '/api': 'http://localhost:3000/',
+      '/login': 'http://localhost:3000/',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './client/index.html' })
+  ]
+};
