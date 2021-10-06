7021168f527cb619a495ba0ff7ada36fcab40c5c
Vince Nguyen <ntgv1993@gmail.com>
2021-10-05 19:11:20 -0500

    Fixed useeffect infinite loop

--- client/components/Feed.jsx
+++ client/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import ReactDOM from 'react-dom';
 import MainContainer from '../containers/MainContainer.jsx';
 import FeedCodeBlock from './FeedCodeBlock.jsx';
@@ -8,22 +8,32 @@ export default function Feed(props) {
   const [codeBlocks, setCodeBlocks] = useState([]);
 
   // update state that we fetch
-  fetch(`/api/getTopic/${props.topic}`)
-    .then((res) => res.json())
-    .then((data) => {
-      setCodeBlocks(data);
-    })
-    .catch((err) => console.log(err));
+  useEffect( () => {
+    fetch(`/api/getTopic/${props.topic}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCodeBlocks(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   // create codeblock components and save them in an array
-  const codeBlockEl = codeBlocks.map(post => {
-    return <FeedCodeBlock key={post._id} code={post.code} />;
-  });
+  // const codeBlockEl = codeBlocks.map(post => {
+  //   return <FeedCodeBlock key={post._id} code={post.code} />;
+  // });
+
+  // mocking up data pending backend fixes
+  const codeBlockEl = [];
+  codeBlockEl.push(<FeedCodeBlock key={1} code={`console.log('a')`} />);
+  codeBlockEl.push(<FeedCodeBlock key={2} code={`console.log('b')`} />);
+  codeBlockEl.push(<FeedCodeBlock key={3} code={`console.log('c')`} />);
 
   // returns code block cards
   return (
     <div>
-      <div>{codeBlockEl}</div>
+      <div>
+        {codeBlockEl}
+      </div>
     </div>
   );
 }
